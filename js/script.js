57b1b1a90ea4b04f192a2b36aeffc83bb2f8e691
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2023-02-06 21:42:46 +0100

    some more logic

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ const buttons = document.getElementsByTagName("button")
 const input_score = document.getElementById("btn-input-score")
 const player_score = document.getElementById("player_score")
 const opponent_score = document.getElementById("opponent_score")
+let player_legs_score = document.getElementById("player_legs_count")
+let player_sets_score = document.getElementById("player_sets_count")
+let opponent_legs_score = document.getElementById("opponent_legs_count")
+let opponent_sets_score = document.getElementById("opponent_sets_count")
+
+const not_checkable = [169, 168, 166, 165, 163, 162, 159]
+let old_player_score = 501
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", function (){
@@ -21,9 +28,29 @@ for(let i = 0; i < buttons.length; i++){
             input_score.classList.add("btn-outline-warning")
         }
         if (buttons[i].value == "okay"){
-            if (parseInt(input_score.innerHTML) >= 0 && parseInt(input_score.innerHTML) <= 180){
+            if (parseInt(input_score.innerHTML) >= 0 && parseInt(input_score.innerHTML) <= 180 && parseInt(player_score.innerHTML) >= 0 && parseInt(player_score.innerHTML) > parseInt(input_score.innerHTML)){
+                old_player_score = parseInt(player_score.innerHTML)
                 player_score.innerHTML = parseInt(player_score.innerHTML) - parseInt(input_score.innerHTML)
                 input_score.innerHTML = 0;
+            }
+        }
+        if (buttons[i].classList.contains("hotkey")){
+            if(parseInt(player_score.innerHTML) > parseInt(buttons[i].value)){
+                old_player_score = parseInt(player_score.innerHTML)
+                player_score.innerHTML = parseInt(player_score.innerHTML) - parseInt(buttons[i].value)
+            }
+        }
+        if (buttons[i].value == "undo"){
+            input_score.innerHTML = 0;
+            input_score.classList.add("btn-outline-dark")
+            input_score.classList.remove("btn-outline-warning")
+            player_score.innerHTML = old_player_score
+        }
+        if (buttons[i].value == "togo"){
+            if (parseInt(player_score.innerHTML) <= 170 && !not_checkable.contains(parseInt(player_score.innerHTML))){
+                player_score.innerHTML = 501
+                opponent_score.innerHTML = 501
+                player_legs_score.innerHTML += 1
             }
         }
     })
