4a0fd767927b3cc394a77265067b5d1a572b4fcd
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2023-02-08 14:56:01 +0100

    implement leg and overall average

--- js/script.js
+++ js/script.js
@@ -9,12 +9,16 @@ let opponent_sets_score = document.getElementById("opponent_sets_count")
 let player_last_score = document.getElementById("player_last")
 let opponent_last_score = document.getElementById("opponent_last")
 let player_average_leg = document.getElementById("player_average_leg")
+let player_average_overall = document.getElementById("player_average_overall")
 
 const not_checkable = [169, 168, 166, 165, 163, 162, 159]
 let old_player_score = []
 let player_rounds = 0
 let player_rounds_leg = 0
 let player_last_scored = [0]
+let player_leg_scored = [0]
+let player_scored_leg = 0
+let player_scored = 0
 let player_leg_average = []
 let player_overall_average = []
 
@@ -40,12 +44,17 @@ for(let i = 0; i < buttons.length; i++){
                 old_player_score.push(parseInt(player_score.innerHTML))
                 player_score.innerHTML = parseInt(player_score.innerHTML) - parseInt(input_score.innerHTML)
                 player_last_scored.push(parseInt(input_score.innerHTML))
+                player_leg_scored.push(parseInt(input_score.innerHTML))
                 player_last_score.innerHTML = player_last_scored[player_last_scored.length - 1]
                 input_score.innerHTML = 0
                 player_rounds += 1
                 player_rounds_leg += 1
-                player_leg_average = (501 - parseInt(player_score.innerHTML)) / player_rounds_leg
+                player_scored_leg = player_leg_scored.reduce(function (a,b){return a+b;})
+                player_scored = player_last_scored.reduce(function (a,b){return a+b;})
+                player_leg_average = player_scored_leg / player_rounds_leg
                 player_average_leg.innerHTML = parseFloat(player_leg_average).toFixed(2);
+                player_overall_average = player_scored / player_rounds
+                player_average_overall.innerHTML = parseFloat(player_overall_average).toFixed(2);
             }
         }
         if (buttons[i].classList.contains("hotkey")){
@@ -53,11 +62,16 @@ for(let i = 0; i < buttons.length; i++){
                 old_player_score.push(parseInt(player_score.innerHTML))
                 player_score.innerHTML = parseInt(player_score.innerHTML) - parseInt(buttons[i].value)
                 player_last_scored.push(parseInt(buttons[i].value))
+                player_leg_scored.push(parseInt(buttons[i].value))
                 player_last_score.innerHTML = player_last_scored[player_last_scored.length - 1]
                 player_rounds += 1
                 player_rounds_leg += 1
-                player_leg_average = (501 - parseInt(player_score.innerHTML)) / player_rounds_leg
+                player_scored_leg = player_leg_scored.reduce(function (a,b){return a+b;})
+                player_scored = player_last_scored.reduce(function (a,b){return a+b;})
+                player_leg_average = player_scored_leg / player_rounds_leg
                 player_average_leg.innerHTML = parseFloat(player_leg_average).toFixed(2);
+                player_overall_average = player_scored / player_rounds
+                player_average_overall.innerHTML = parseFloat(player_overall_average).toFixed(2);
             }
         }
         if (buttons[i].value == "undo"){
@@ -77,11 +91,22 @@ for(let i = 0; i < buttons.length; i++){
         if (buttons[i].value == "togo"){
             if (parseInt(player_score.innerHTML) <= 170 && !not_checkable.includes(parseInt(player_score.innerHTML))){
                 old_player_score.push(parseInt(player_score.innerHTML))
+                player_last_scored.push(parseInt(player_score.innerHTML))
+                player_leg_scored.push(parseInt(player_score.innerHTML))
                 player_score.innerHTML = 501
                 opponent_score.innerHTML = 501
                 player_legs_score.innerHTML = parseInt(player_legs_score.innerHTML) + 1
                 player_last_scored.push(0)
                 player_last_score.innerHTML = player_last_scored[player_last_scored.length - 1]
+                player_rounds += 1
+                player_rounds_leg = 0
+                player_scored_leg = player_leg_scored.reduce(function (a,b){return a+b;})
+                player_scored = player_last_scored.reduce(function (a,b){return a+b;})
+                player_leg_scored = [0]
+                player_leg_average = 0
+                player_average_leg.innerHTML = parseFloat(player_leg_average).toFixed(2);
+                player_overall_average = player_scored / player_rounds
+                player_average_overall.innerHTML = parseFloat(player_overall_average).toFixed(2);
             }
         }
     })
