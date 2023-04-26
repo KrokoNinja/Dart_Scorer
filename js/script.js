6788e26aac105905ab4a498b307734463bc9a954
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2023-04-26 18:29:27 +0200

    implemented some bugs so it dont gets boring xD-.-

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const buttons = document.getElementsByTagName("button");
 const input_score = document.getElementById("btn-input-score");
 const not_checkable = [169, 168, 166, 165, 163, 162, 159];
+let turn = "player";
+let winner = undefined;
 
 //Player stats
 const player_score = document.getElementById("player_score");
+const player_area = document.getElementById("player_area");
 let player_legs_score = document.getElementById("player_legs_count");
 let player_sets_score = document.getElementById("player_sets_count");
 let player_last_score = document.getElementById("player_last");
@@ -18,13 +21,12 @@ let player_overall_average = [];
 
 //Opponent stats
 const opponent_score = document.getElementById("opponent_score");
+const opponent_area = document.getElementById("opponent_area");
 let opponent_legs_score = document.getElementById("opponent_legs_count");
 let opponent_sets_score = document.getElementById("opponent_sets_count");
 let opponent_last_score = document.getElementById("opponent_last");
 let opponent_average_leg = document.getElementById("opponent_average_leg");
-let opponent_average_overall = document.getElementById(
-	"opponent_average_overall"
-);
+let opponent_average_overall = document.getElementById("opponent_average_overall");
 let opponent_rounds = 0;
 let old_opponent_score = [];
 let opponent_rounds_leg = [0];
@@ -52,7 +54,14 @@ function doubleTapPreventZoom(event) {
 	lastTouchEnd = now;
 }
 
-document.addEventListener("keyup", (pressed) => {
+function newGame() {
+	document.addEventListener("keyup", (pressed) => key_pressed(pressed));
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener("click", button_clicked(buttons[i]));
+	}
+}
+
+function key_pressed(pressed) {
 	for (let i = 0; i < buttons.length; i++) {
 		buttons[i].blur();
 	}
@@ -80,81 +89,75 @@ document.addEventListener("keyup", (pressed) => {
 			input_score.classList.remove("btn-outline-warning");
 		}
 	}
-	if (
-		code == "Enter" &&
-		!input_score.classList.contains("btn-outline-warning")
-	) {
+	if (code == "Enter" && !input_score.classList.contains("btn-outline-warning")) {
 		buttonPressed("okay");
 	}
-});
+}
 
-for (let i = 0; i < buttons.length; i++) {
-	buttons[i].addEventListener("click", function () {
+function button_clicked(button) {
+	if (
+		!button.classList.contains("hotkey") &&
+		input_score.innerHTML.trim() != "0" &&
+		button.value != "togo" &&
+		button.value != "undo" &&
+		button.value != "okay"
+	) {
+		input_score.innerHTML += button.value;
+	} else if (
+		!button.classList.contains("hotkey") &&
+		button.value != "togo" &&
+		button.value != "undo" &&
+		button.value != "okay"
+	) {
+		input_score.innerHTML = button.value;
+	}
+	if (parseInt(input_score.innerHTML) > 180) {
+		input_score.classList.remove("btn-outline-dark");
+		input_score.classList.add("btn-outline-warning");
+	}
+	if (button.value == "clear") {
+		buttonPressed("clear");
+	}
+	if (button.value == "okay") {
 		if (
-			!buttons[i].classList.contains("hotkey") &&
-			input_score.innerHTML.trim() != "0" &&
-			buttons[i].value != "togo" &&
-			buttons[i].value != "undo" &&
-			buttons[i].value != "okay"
+			parseInt(input_score.innerHTML) >= 0 &&
+			parseInt(input_score.innerHTML) <= 180 &&
+			parseInt(player_score.innerHTML) >= 0 &&
+			parseInt(player_score.innerHTML) > parseInt(input_score.innerHTML)
 		) {
-			input_score.innerHTML += buttons[i].value;
-		} else if (
-			!buttons[i].classList.contains("hotkey") &&
-			buttons[i].value != "togo" &&
-			buttons[i].value != "undo" &&
-			buttons[i].value != "okay"
+			buttonPressed("okay");
+		}
+	}
+	if (button.classList.contains("hotkey")) {
+		if (parseInt(player_score.innerHTML) > parseInt(button.value)) {
+			buttonPressed("hotkey", button.value);
+		}
+	}
+	if (button.value == "undo") {
+		buttonPressed("undo");
+	}
+	if (button.value == "togo") {
+		if (
+			parseInt(player_score.innerHTML) <= 170 &&
+			!not_checkable.includes(parseInt(player_score.innerHTML))
 		) {
-			input_score.innerHTML = buttons[i].value;
+			newLeg();
 		}
-		if (parseInt(input_score.innerHTML) > 180) {
-			input_score.classList.remove("btn-outline-dark");
-			input_score.classList.add("btn-outline-warning");
-		}
-		if (buttons[i].value == "clear") {
-			buttonPressed("clear");
-		}
-		if (buttons[i].value == "okay") {
-			if (
-				parseInt(input_score.innerHTML) >= 0 &&
-				parseInt(input_score.innerHTML) <= 180 &&
-				parseInt(player_score.innerHTML) >= 0 &&
-				parseInt(player_score.innerHTML) > parseInt(input_score.innerHTML)
-			) {
-				buttonPressed("okay");
-			}
-		}
-		if (buttons[i].classList.contains("hotkey")) {
-			if (parseInt(player_score.innerHTML) > parseInt(buttons[i].value)) {
-				buttonPressed("hotkey", buttons[i].value);
-			}
-		}
-		if (buttons[i].value == "undo") {
-			buttonPressed("undo");
-		}
-		if (buttons[i].value == "togo") {
-			if (
-				parseInt(player_score.innerHTML) <= 170 &&
-				!not_checkable.includes(parseInt(player_score.innerHTML))
-			) {
-				newLeg();
-			}
-		}
-	});
+	}
 }
 
 /**
  * Set button actions for the special keys "okay", "undo", "hotkey" & "clear"
- * @param {String} key
+ * @param {String} key of the button which was pressed
+ * @param {String} value of the hotkey if the button was a hotkey button
  */
-function buttonPressed(key) {
+function buttonPressed(key, value = 0) {
 	switch (key) {
 		case "okay":
 			old_player_score.push(parseInt(player_score.innerHTML));
-			player_score.innerHTML =
-				parseInt(player_score.innerHTML) - parseInt(input_score.innerHTML);
+			player_score.innerHTML = parseInt(player_score.innerHTML) - parseInt(input_score.innerHTML);
 			player_last_scored.push(parseInt(input_score.innerHTML));
-			player_last_score.innerHTML =
-				player_last_scored[player_last_scored.length - 1];
+			player_last_score.innerHTML = player_last_scored[player_last_scored.length - 1];
 			input_score.innerHTML = 0;
 			calculateAverage("okay");
 			break;
@@ -163,11 +166,10 @@ function buttonPressed(key) {
 			break;
 		case "hotkey":
 			old_player_score.push(parseInt(player_score.innerHTML));
-			player_score.innerHTML =
-				parseInt(player_score.innerHTML) - parseInt(value);
+			player_score.innerHTML = parseInt(player_score.innerHTML);
 			player_last_scored.push(parseInt(value));
 			player_last_score.innerHTML =
-				player_last_scored[player_last_scored.length - 1];
+				player_last_scored[player_last_scored.length - 1] - parseInt(value);
 			input_score.innerHTML = 0;
 			calculateAverage("hotkey");
 			break;
@@ -183,15 +185,14 @@ function buttonPressed(key) {
  * Reset stats for a new Leg
  */
 function newLeg() {
-	old_player_score.push(parseInt(player_score.innerHTML));
-	player_last_scored.push(parseInt(player_score.innerHTML));
 	player_score.innerHTML = 501;
 	opponent_score.innerHTML = 501;
 	player_legs_score.innerHTML = parseInt(player_legs_score.innerHTML) + 1;
-	player_last_score.innerHTML =
-		player_last_scored[player_last_scored.length - 1];
-	player_rounds += 1;
+	player_last_score.innerHTML = player_last_scored[player_last_scored.length - 1];
 	player_rounds_leg.push(0);
+	old_player_score.push(parseInt(player_score.innerHTML));
+	player_last_scored.push(parseInt(player_score.innerHTML));
+	player_rounds += 1;
 	player_leg_average = 0;
 	calculateAverage("togo");
 }
@@ -210,21 +211,14 @@ function undoScore() {
 		player_score.innerHTML = old_player_score[old_player_score.length - 1];
 		old_player_score.pop();
 		player_last_scored.pop();
-		player_last_score.innerHTML =
-			player_last_scored[player_last_scored.length - 1];
+		player_last_score.innerHTML = player_last_scored[player_last_scored.length - 1];
 		player_rounds -= 1;
 		if (player_rounds_leg.length >= parseInt(player_legs_score.innerHTML) + 2) {
 			player_rounds_leg.pop();
-		} else if (
-			player_rounds_leg[parseInt(player_legs_score.innerHTML)] - 1 >=
-			0
-		) {
+		} else if (player_rounds_leg[parseInt(player_legs_score.innerHTML)] - 1 >= 0) {
 			player_rounds_leg[parseInt(player_legs_score.innerHTML)] -= 1;
 		}
-		if (
-			player_rounds_leg[parseInt(player_legs_score.innerHTML)] == 0 &&
-			player_rounds != 0
-		) {
+		if (player_rounds_leg[parseInt(player_legs_score.innerHTML)] == 0 && player_rounds != 0) {
 			player_leg_average = 0;
 		} else if (player_rounds == 0) {
 			player_leg_average = 0;
@@ -248,14 +242,13 @@ function calculateAverage(key) {
 	});
 	if (player_rounds_leg[parseInt(player_legs_score.innerHTML)] != 0) {
 		player_leg_average =
-			(501 - player_score.innerHTML) /
-			player_rounds_leg[parseInt(player_legs_score.innerHTML)];
+			(501 - player_score.innerHTML) / player_rounds_leg[parseInt(player_legs_score.innerHTML)];
 	}
 	if (player_rounds != 0) {
 		player_overall_average = player_scored / player_rounds;
 	}
 	player_average_leg.innerHTML = parseFloat(player_leg_average).toFixed(2);
-	player_average_overall.innerHTML = parseFloat(player_overall_average).toFixed(
-		2
-	);
+	player_average_overall.innerHTML = parseFloat(player_overall_average).toFixed(2);
 }
+
+newGame();
